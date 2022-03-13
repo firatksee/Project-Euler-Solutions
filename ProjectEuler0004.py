4632667428b76ea77a8b95dc739dcb2ef798bc25
Fırat Köse <70978970+firatksee@users.noreply.github.com>
2022-03-13 13:22:08 +0300

    Update ProjectEuler0004.py

--- ProjectEuler0004.py
+++ ProjectEuler0004.py
@@ -6,11 +6,11 @@
 #github.com/firatksee
 
 
-def largestPalindrome():
-	result = (m*n for m in range(100,1000)
-			for n in range(100,1000)
+def largestPalindrome(start, stop):
+	result = (m*n for m in range(start, stop)
+			for n in range(start, stop)
 			if str(m*n) == str(m*n)[::-1])
 	return result
 
 if __name__ == "__main__":
-	print(max(largestPalindrome()))
+	print(max(largestPalindrome(100, 1000)))
