3628a58c4bdb2bf6e81c240368b3a25006226b5f
Fırat Köse <70978970+firatksee@users.noreply.github.com>
2022-03-14 14:43:13 +0300

    Create ProjectEuler0010.py

---
+++ ProjectEuler0010.py
@@ -0,0 +1,25 @@
+
+#projecteuler.net/problem=10
+
+#Answer: 142913828922
+
+#github.com/firatksee
+
+
+def isPrime(number):
+	if number < 2: return False
+	if number == 2: return True
+	if number % 2 == 0: return False
+
+	for i in range(3, int(number ** 0.5) + 1, 2):
+		if number % i == 0: return False
+	return True
+
+
+def sumPrimesBelow(n):
+	result = sum(i for i in range(n) if isPrime(i))
+	return result
+
+
+if __name__ == "__main__":
+	print(sumPrimesBelow(2000000))
