69bfa5e7df404276fdcbab17ae65ea30eb74112c
Fırat Köse <70978970+firatksee@users.noreply.github.com>
2023-10-05 18:54:04 +0300

    Minor changes

--- JavaScript/ProjectEuler0007.js
+++ JavaScript/ProjectEuler0007.js
@@ -9,9 +9,7 @@ function isPrime(number) {
     if (number === 2) return true;
     if (number % 2 === 0) return false;
 
-    const limit = Math.floor(Math.sqrt(number));
-
-    for (let i = 3; i <= limit; i += 2) {
+    for (let i = 3; i <= Math.sqrt(number); i += 2) {
         if (number % i === 0) return false;
     }
     return true;
